45578ed2ee8070dd25339a1ef7967871ccd9f48e
Mikhailov Alexey <a.mikhailov@flydata.ru>
2021-06-07 12:49:44 +0300

    Initial Commit

---
+++ PerforatedTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PerforatedTable
+//
+//  Created by Alexey Mikhailov on 07.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
